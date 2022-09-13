f59a9d5d225c57cdc5b579aa1ee28af45dad424a
Jake Schaefbauer <jschaefmn@gmail.com>
2022-09-12 22:25:29 -0500

    finished lesson 1

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,31 @@
+const { User, Thought} = require('../models');
+
 //with query set up add resolver that will server the response for helloWorld query
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello world!';
-    }
+    // Pass in parent as more of a placeholder parameter and not used
+    // use ternary operator to check if username exists, if it does then filter by username, if not then return empty array
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find().sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // get all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
   }
 };
 
